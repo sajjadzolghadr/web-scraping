125d33a1ed00b4175aa1ba5e2624adf5ebe02633
sajjadzolghadr77 <sajjadzolghadr777@gmail.com>
2025-04-01 21:16:23 +0330

    Web Scraping books

---
+++ book_to_scrape.py
@@ -0,0 +1,16 @@
+import requests
+import bs4
+base_url = 'https://books.toscrape.com/catalogue/page-{}.html'
+three_star_title = []
+for n in range(1,51):
+    scrape_url = base_url.format(n)
+    results = requests.get(scrape_url)
+    soup = bs4.BeautifulSoup(results.text , 'lxml')
+    books = soup.select(".product_dot")
+
+    for book in books:
+        if len(book.select('.star-rating.Three'))!= 0:
+            book_title = book.select('a')[1]['title']
+            three_star_title.append(book_title)
+
+print(three_star_title)
